05404b26d45d2a363dd4e693288238ea289bafa3
dotnetCarpenter <jon.ronnenberg@gmail.com>
2021-08-16 01:50:48 +0200

    got basic cli/markdown table working

--- matrix.js
+++ matrix.js
@@ -1,47 +1,78 @@
 'use strict'
 
-const typeClasses = require ('fantasy-land')
-const S           = require ('sanctuary')
+const Z = require ('sanctuary-type-classes')
+const S = require ('sanctuary')
+
+/*--------------- General functions */
+const trace = msg => x => (console.log (msg, x), x);
+
+const isTypeClass = x => x['@@type'] === 'sanctuary-type-classes/TypeClass@1';
+
+const sillyHead = ([name]) => name;
+
+const sillyLength = s => s.length;
+
+
+
+/*--------------- Model */
 
 const adts = [
-  ['Pair'  , [S.Pair ('') ('')]],
-  ['Maybe' , [S.Just (''), S.Nothing]],
-  ['Either', [S.Right (''), S.Left]],
-]
+  ['Pair'  , S.Pair ('') ('')],
+  ['Maybe' , S.Just ('')],
+  ['Either', S.Right ('')],
+];
 
-const trace = msg => x => (console.log (msg, x), x)
+const SUPPORTED = '\u2705';
+const UNSUPPORTED = '\u274C';
 
-const getSupportedClasses = S.flip (adt => S.filter (tc => !!adt[tc])) (typeClasses)
+const typeClasses = S.keys (S.unchecked.filter (isTypeClass) (Z));
 
-const matrix = adts.map (([name, dataTypes]) => [name, dataTypes.map (getSupportedClasses)])
+// getTypeClassSupportForAdt :: String -> Array (Array (a Adt)) -> Array (String)
+const getTypeClassSupportForAdts = typeClass => S.unchecked.pipe ([
+  S.unchecked.map (([_, adt]) => Z[typeClass].test (adt)),
+  S.map (S.boolean (UNSUPPORTED) (SUPPORTED)),
+  S.prepend (typeClass)
+]);
 
-const sillyHead = ([name]) => name
+// typeClassTests :: Array (Adt -> Array (String))
+const typeClassTests = S.map (getTypeClassSupportForAdts) (typeClasses);
 
-const sillyLength = s => s.length
 
-const pipeJoin = S.joinWith (' | ')
+// Unused
+const getAdtTypeClassSupport = adt => S.pipe ([
+  S.map (typeClass => Z[typeClass].test (adt)),
+  S.map (S.boolean (UNSUPPORTED) (SUPPORTED)),
+]);
 
-const header = S.compose (pipeJoin) (S.unchecked.map (sillyHead))
+
+
+/*--------------- View */
+const pipeJoin = S.joinWith (' | ');
+
+const header = S.compose (pipeJoin) (S.unchecked.map (sillyHead));
 
 const headerAlignment = S.unchecked.pipe ([
   S.unchecked.map (S.compose (sillyLength) (sillyHead)),
   S.map (n => '-'.repeat (n)),
   pipeJoin
-])
+]);
 
-const rows = S.I
+const padEnd = n => s => s.padEnd (n);
 
-const view = adts => matrix =>
+const row = padding =>
+  S.compose
+    (S.map (pipeJoin))
+    (S.map (S.zipWith (padEnd) (padding)));
+
+const view = adts => typeClassTests =>
 `
-| Type Class | ${header (adts)} |
-| ---------- | ${headerAlignment (adts)} |
+|  Type Class   | ${header (adts)} |
+| ------------  | ${headerAlignment (adts)} |
+${S.joinWith ('\n')
+             (S.map (r => `| ${r} |`)
+                    (row ([13, 3, 4, 5]) (S.map (test => test (adts))
+                                                (typeClassTests))))}
+`;
 
-`
-
-// console.log (typeClasses)
-
-console.log (view (adts) (matrix))
-
-console.log (
-	getSupportedClasses (S.Just (''))
-)
+/*--------------- Impure */
+console.log (view (adts) (typeClassTests));
